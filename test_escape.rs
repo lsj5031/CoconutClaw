7fd263707afd6e12a0d263ec659ae52c2afb63d9
Leo <lsj5031@gmail.com>
2026-02-28 19:15:18 +1300

    chore: remove stray test file

--- test_escape.rs
+++
@@ -1,7 +0,0 @@
-fn main() {
-    let text1 = "Special chars: _ * [ ] ( ) ~ ` > # + - = | { } . !";
-    match telegram_markdown_v2::convert_with_strategy(text1, telegram_markdown_v2::UnsupportedTagsStrategy::Escape) {
-        Ok(rendered) => println!("Rendered: {}", rendered),
-        Err(err) => println!("Error: {}", err),
-    }
-}
